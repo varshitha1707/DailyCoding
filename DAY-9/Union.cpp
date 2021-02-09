bb4ce2de527283aed02ba0c67978271bf9b2696b
varshitha1707 <69676094+varshitha1707@users.noreply.github.com>
2021-02-09 13:31:19 +0530

    Add files via upload

---
+++ DAY-9/Union.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+void Union(int ar1[], int n1, int ar2[], int n2)
+{
+
+    int i=0, j=0;
+   while(i<n1 && j<n2)
+   {
+       if(ar1[i]<ar2[j])
+       {
+           cout<<ar1[i++]<<" ";
+
+
+       }
+       else if( ar2[j]<ar1[i])
+       {
+           cout<<ar2[j++]<<" ";
+
+       }
+       else
+       {
+          cout<<ar1[i++]<<" ";
+          j++;
+       }
+
+   }
+
+   while(i<n1)
+   {
+       cout<<ar1[i++]<<" ";
+   }
+   while(j<n2)
+   {
+       cout<<ar2[j++]<<" ";
+   }
+}
+
+int main()
+{
+   int n1,n2;
+   cout << "Enter the size of the first array" << endl;
+   cin>>n1;
+   int ar1[n1];
+   cout<<"Enter the elements of the first array:";
+   for(int i = 0; i<n1; i++)
+   {
+       cin>>ar1[i];
+   }
+   cout<<"Enter the size of the second array";
+   cin>>n2;
+   int ar2[n2];
+   cout<<"Enter the elements of the second array";
+   for(int i=0; i<n2; i++)
+   {
+       cin>>ar2[i];
+   }
+
+   Union(ar1,n1,ar2,n2);
+
+    return 0;
+}
