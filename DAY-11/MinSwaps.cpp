389feac616d0cb775dd4ebf706b22ea53872b65e
varshitha1707 <69676094+varshitha1707@users.noreply.github.com>
2021-02-11 23:48:35 +0530

    Add files via upload

---
+++ DAY-11/MinSwaps.cpp
@@ -0,0 +1,68 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int minSwaps(int arr[], int n)
+{
+    pair<int, int> arrPos[n];
+    for (int i = 0; i < n; i++)
+    {
+        arrPos[i].first = arr[i];
+        arrPos[i].second = i;
+    }
+
+    sort(arrPos, arrPos + n);
+
+    vector<bool> vis(n, false);
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (vis[i] || arrPos[i].second == i)
+            continue;
+
+        int cycle_size = 0;
+        int j = i;
+        while (!vis[j])
+        {
+            vis[j] = 1;
+
+            j = arrPos[j].second;
+            cycle_size++;
+        }
+
+
+        if (cycle_size > 0)
+        {
+            ans += (cycle_size - 1);
+        }
+    }
+
+
+    return ans;
+}
+
+
+int main()
+{
+    int n ;
+    cout<<"Enter n\n";
+    cin>>n;
+    int arr[n];
+
+    cout<<"Enter elements:\n";
+    for(int a=0; a<n;a++) {
+        cin >> arr[a];
+    }
+    cout<<"Sorted Array:\n";
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\nMin Swaps: ";
+    cout << minSwaps(arr, n);
+    return 0;
+}
